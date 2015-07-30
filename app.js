74c971456a8cac61f78e2409cc46d952dc6d13da
logan dougher <logan.dougher@gmail.com>
2015-07-30 15:25:00 -0500

    added test, need to write log function for req/res data, added start in package.json, installed heroku

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var chalk = require('chalk');
+//console.log(chalk.blue.underline.bold('hello world'));
 require('./lib/server')(1337);
 
-console.log('Server running on http://localhost:1337');
+
+console.log('Server running on ' + chalk.blue.underline.bold('http://localhost:1337'));
--- test/unit/calURLParse.js
+++ test/unit/calURLParse.js
@@ -1,4 +1,4 @@
-var expect = require('chai').expect;
+/*var expect = require('chai').expect;
 var path = require('path');
 var parse = require(path.join(process.cwd(), '/lib/calURLParse'));
 
@@ -23,5 +23,39 @@ describe('calURLParse', function () {
   it('should handle invalue routes', function () {
     expect(parse('/cal/foo')).to.equal('./cal foo');
     expect(parse('/cal/foo/bar')).to.equal('./cal foo bar');
+  });
+});*/
+
+var expect = require('chai').expect;
+var path = require('path');
+var log = require(path.join(process.cwd(), '/lib/log'));
+var server = require(process.cwd() + '/lib/server');
+
+describe('log', function () {
+  it('should create the log', function () {
+    var date = (new Date()).toUTCString();
+    var req = {
+      method: 'GET',
+      url: '/starwarsmovies',
+      headers: {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36'
+      }
+    var res = {statusCode : 200}
+    };
+
+    /*var req2 = {
+      method: 'POST',
+      url: '/my/form',
+      headers: {
+        'user-agent': 'curl/7.43.0'
+      }
+    };*/
+
+    var goal1 = '[' + date + '] "\u001b[36mGET\u001b[39m \u001b[36m/starwarsmovies\u001b[39m" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36"';
+    //var goal2 = '[' + date + '] "\u001b[36mPOST\u001b[39m \u001b[36m/my/form\u001b[39m" "curl/7.43.0"';
+    expect(log(req)).to.equal(goal1);
+    //expect(log(req2)).to.equal(goal2);
+
+
   });
 });
